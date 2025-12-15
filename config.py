1f97fe4602b45624c677f629554f96a83da04a0f
Artur <arturnazaryan@macbookpro.attlocal.net>
2025-12-15 10:13:52 -0800

    upload workflow

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 class DevelopmentConfig:
   SQLALCHEMY_DATABASE_URI = 'sqlite:///mechanic.db'
   DEBUG = True
+  CACHE_TYPE = 'SimpleCache'
+  CACHE_DEFAULT_TIMEOUT = 300
 
 class TestingConfig:
   SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
--- tests/tests_customers.py
+++ tests/tests_customers.py
@@ -1,68 +1,68 @@
 from app import create_app
-from app.models import db
-from werkzeug.security import check_password_hash, generate_password_hash
+from app.models import Customers, db
 import unittest
-from app.utils.util import encode_token
-
-class TestCustomer(unittest.TestCase):
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.utils import encode_token
+class TestCustomers(unittest.TestCase):
+    
     def setUp(self):
         self.app = create_app("TestingConfig")
-        self.customer = Customers(email='', username='', password='', role='')
+        self.customer= Customers(email='tester@gmail.com', firstName='greg', lastName='jef', phone='210-222-2222', address='123 fun st', password=generate_password_hash('123') )
         with self.app.app_context():
             db.drop_all()
             db.create_all()
             db.session.add(self.customer)
-            db.session.comit() 
+            db.session.commit()
         self.token = encode_token(1, 'customer')
-        self.client = self.app.test_cllient()
+        self.client = self.app.test_client()
+
     def test_create_customer(self):
         customer_payload = {
-            
-            "firstName": "test_customer",
-            "email": "test@email.com",
-           	"DOB": "1900-01-01",
-            "role": "admin",
-            "password": "123",
-            "address" : "123 fun st."
+            "firstName": "david",
+            "lastName": 'jeff',
+            "email" : "test@email.com",
+            "phone": "220-222-2222",
+            "address" : "123 fun st.",
+            "password" : '12334'
         }
-        response = self.client.post('/customers/', json=customer_payload)
-        print(response.json)
+        response = self.client.post('/customers', json=customer_payload)
+        print(response)
         self.assertEqual(response.status_code, 201)
-        self.assertIn(response.json['email'], "response.json")
-
-    def test_invalid_customer(self):
+        self.assertEqual(response.json['email'], 'test_customer')
+        self.assertTrue(check_password_hash(response.json['password'], '12334'))
+    
+    def test_invalid_create(self):
         customer_payload = {
-            
-            "firstName": "test_customer",
-           	"DOB": "1900-01-01",
-            "role": "admin",
-            "password": "123",
-            "address" : "123 fun st."
+            "lastName": 'jeff',
+            "email" : "test@email.com",
+            "phone": "220-222-2222",
+            "address" : "123 fun st.",
+            "password" : '12334'
         }
-        response = self.client.post('/customers/', json=customer_payload)
-        print(response.json)
+        response = self.client.post('/customers', json=customer_payload)
         self.assertEqual(response.status_code, 400)
-        self.assertIn(response.json['email'], "response.json")
+        self.assertIn('firstName', response.json)
+    
+    def test_get_customers(self):
+        response = self.client.get('/customers')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json[0]['email'], 'tester')
 
+   
     def test_login(self):
       login_creds = {
-        "email": "tester",
+        "email": "tester@gmail.com",
         "password": '123'
       }
 
       response = self.client.post('/customers/login', json =login_creds)
       self.assertEqual(response.status_code, 200)
       self.assertIn('auth_token', response.json)
-
-    def test_get_customers(self):
-      response = self.client.get('/customers')
-      self.assertEqual(response.status_code, 200)
-      self.assertEqual(response.json [0]['username'], 'tester')
   
     def test_delete(self):
-      headers = {'auhotization': "bearer " + self.token}
+      headers = {'authorization': "bearer " + self.token}
 
-      response = self.client.delete('/users', headers.headers)
+      response = self.client.delete('/customers', headers.headers)
       self.assertEqual(response.status_code, 201)
       self.assertEqual(response.json['message'], 'succesfully deleted user 1')
     
