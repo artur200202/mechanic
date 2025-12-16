8fe71913fae04e85a532137ffefbe5914d8929f5
Artur <arturnazaryan@macbookpro.attlocal.net>
2025-12-15 16:34:01 -0800

    changed name

--- tests/test_*.py
+++ tests/test_*.py
@@ -3,11 +3,20 @@ from app.models import Customers, db
 import unittest
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.utils import encode_token
-class TestCustomers(unittest.TestCase):
+
+
+class CustomerTestCase(unittest.TestCase):
     
     def setUp(self):
         self.app = create_app("TestingConfig")
-        self.customer= Customers(email='tester@gmail.com', firstName='greg', lastName='jef', phone='210-222-2222', address='123 fun st', password=generate_password_hash('123') )
+        self.customer = Customers(
+            email='tester@gmail.com', 
+            firstName='greg', 
+            lastName='jef', 
+            phone='210-222-2222', 
+            address='123 fun st', 
+            password=generate_password_hash('123')
+        )
         with self.app.app_context():
             db.drop_all()
             db.create_all()
@@ -20,24 +29,25 @@ class TestCustomers(unittest.TestCase):
         customer_payload = {
             "firstName": "david",
             "lastName": 'jeff',
-            "email" : "test@email.com",
+            "email": "test@email.com",
             "phone": "220-222-2222",
-            "address" : "123 fun st.",
-            "password" : '12334'
+            "address": "123 fun st.",
+            "password": '12334'
         }
         response = self.client.post('/customers', json=customer_payload)
         print(response)
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json['email'], 'test_customer')
+        
+        self.assertEqual(response.json['email'], 'test@email.com')
         self.assertTrue(check_password_hash(response.json['password'], '12334'))
     
     def test_invalid_create(self):
         customer_payload = {
             "lastName": 'jeff',
-            "email" : "test@email.com",
+            "email": "test@email.com",
             "phone": "220-222-2222",
-            "address" : "123 fun st.",
-            "password" : '12334'
+            "address": "123 fun st.",
+            "password": '12334'
         }
         response = self.client.post('/customers', json=customer_payload)
         self.assertEqual(response.status_code, 400)
@@ -46,23 +56,24 @@ class TestCustomers(unittest.TestCase):
     def test_get_customers(self):
         response = self.client.get('/customers')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json[0]['email'], 'tester')
+      
+        self.assertEqual(response.json[0]['email'], 'tester@gmail.com')
 
-   
     def test_login(self):
-      login_creds = {
-        "email": "tester@gmail.com",
-        "password": '123'
-      }
-
-      response = self.client.post('/customers/login', json =login_creds)
-      self.assertEqual(response.status_code, 200)
-      self.assertIn('auth_token', response.json)
+        login_creds = {
+            "email": "tester@gmail.com",
+            "password": '123'
+        }
+        response = self.client.post('/customers/login', json=login_creds)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('auth_token', response.json)
   
     def test_delete(self):
-      headers = {'authorization': "bearer " + self.token}
+        headers = {'authorization': "bearer " + self.token}
+        response = self.client.delete('/customers', headers=headers)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json['message'], 'succesfully deleted user 1')
 
-      response = self.client.delete('/customers', headers.headers)
-      self.assertEqual(response.status_code, 201)
-      self.assertEqual(response.json['message'], 'succesfully deleted user 1')
-    
+
+if __name__ == '__main__':
+    unittest.main()
